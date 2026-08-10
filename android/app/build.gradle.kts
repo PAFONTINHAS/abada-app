3d17c5d718a36b6c5da1bb5955bd528b6c4b3c93
Peterson Fontinhas <petersonfontinhas@gmail.com>
2026-08-09 23:25:19 -0300

    firebase initialized

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    id("com.android.application")
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -38,6 +42,19 @@ android {
         }
     }
 }
+
+dependencies {
+
+  implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
+  implementation("com.google.firebase:firebase-firestore")
+  implementation("com.google.firebase:firebase-functions")
+  implementation("com.google.firebase:firebase-messaging")
+  implementation("com.google.firebase:firebase-storage")
+
+
+
+}
+
 
 flutter {
     source = "../.."
