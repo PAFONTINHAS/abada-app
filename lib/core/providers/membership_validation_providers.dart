67321db62556af4514ac006b65513844bb36ab0d
isabellalcvicente <isabellacadastros16@gmail.com>
2026-09-16 21:43:59 -0300

    chore: configs

--- lib/core/providers/membership_validation_providers.dart
+++ lib/core/providers/membership_validation_providers.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:sistema_abada_capoeira/features/member_validation/domain/repository/membership_validation_repostitory.dart';
-
 import '../../features/member_validation/data/datasource/membership_validation_remote_datasource.dart';
 import '../../features/member_validation/data/datasource/membership_validation_remote_datasource_impl.dart';
 import '../../features/member_validation/data/repository/membership_validation_repository_impl.dart';
@@ -16,36 +14,29 @@ class MembershipValidationProvider {
   //Representa como tudo dessa feature é montado
   MembershipValidationProvider._();
 
-  static final MembershipValidationRemoteDataSource remoteDataSource =
-      //quero uma variável seguindo o contrato MembershipValidationRemoteDataSource
-      MembershipValidationRemoteDataSourceImpl();
-  //implementação real. DataSource, aqui está seu telefone para falar com o Firebase
+  static final MembershipValidationRemoteDataSource remoteDataSource = MembershipValidationRemoteDataSourceImpl();
+  //quero uma variável seguindo o contrato MembershipValidationRemoteDataSource
+  //com a implementação real DataSource, aqui está seu telefone para falar com o Firebase
 
-  static final MembershipValidationRepository repository =
-      //crie uma variavel seguindo o contrato
-      MembershipValidationRepositoryImpl(remoteDataSource);
+  static final MembershipValidationRepository repository = MembershipValidationRepositoryImpl(remoteDataSource);
+  //crie uma variavel seguindo o contrato
   // o RepositoryImpl precisa de alguém que realmente fale com o banco
 
-  static final GetProfessorMembershipRequestsUseCase getProfessorRequests =
-      GetProfessorMembershipRequestsUseCase(repository);
+  static final GetProfessorMembershipRequestsUseCase getProfessorRequests = GetProfessorMembershipRequestsUseCase(repository);
   //quando precisar buscar, use esse Repository
 
-  static final ApproveMembershipRequestUseCase approveMembershipRequest =
-      ApproveMembershipRequestUseCase(repository);
+  static final ApproveMembershipRequestUseCase approveMembershipRequest = ApproveMembershipRequestUseCase(repository);
   //quando precisar aprovar, use esse Repository
 
-  static final RequestMembershipChangesUseCase requestMembershipChanges =
-      RequestMembershipChangesUseCase(repository);
+  static final RequestMembershipChangesUseCase requestMembershipChanges = RequestMembershipChangesUseCase(repository);
   //quando precisar de correção, use esse Repository
 
-  static final RejectMembershipRequestUsecase rejectMembershipRequest =
-      RejectMembershipRequestUsecase(repository);
+  static final RejectMembershipRequestUsecase rejectMembershipRequest = RejectMembershipRequestUsecase(repository);
   //quando precisar rejeitar, use esse Repository
 
   static List<SingleChildWidget> providers = [
-    //crie uma lista de Providers
     ChangeNotifierProvider(
-      //“Você será responsável por criar e disponibilizar este Controller.”
+      //“Você será responsável por criar e disponibilizar para a arvore de widgets este Controller.”
       create: (_) => MembershipValidationController(
         //create é uma função, espera parametro/contexto e retorno, o traço significa "existe, mas nao vou usar"
         //quando precisa criar o Provider, faça o seguinte:
--- lib/core/providers/profile_providers.dart
+++ lib/core/providers/profile_providers.dart
@@ -1,7 +1,5 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
-import 'package:sistema_abada_capoeira/core/providers/auth_providers.dart';
-import 'package:sistema_abada_capoeira/features/auth/domain/usecases/logout_user_usecase.dart';
 import 'package:sistema_abada_capoeira/features/profile/data/datasources/profile_remote_datasource.dart';
 import 'package:sistema_abada_capoeira/features/profile/data/datasources/profile_remote_datasource_impl.dart';
 import 'package:sistema_abada_capoeira/features/profile/data/repository/profile_repository_impl.dart';
@@ -16,19 +14,29 @@ import 'package:sistema_abada_capoeira/features/profile/presentation/controllers
 import 'package:sistema_abada_capoeira/features/profile/presentation/controllers/request_status_controller.dart';
 
 class ProfileProviders {
-
   ProfileProviders._();
 
-  static final ProfileRemoteDatasource profileRemoteDatasource = ProfileRemoteDatasourceImpl();
-  static final ProfileRepository profileRepository = ProfileRepositoryImpl(profileRemoteDatasource);
+  static final ProfileRemoteDatasource profileRemoteDatasource =
+      ProfileRemoteDatasourceImpl();
+  static final ProfileRepository profileRepository = ProfileRepositoryImpl(
+    profileRemoteDatasource,
+  );
 
-  static final UpdateProfileInfoUseCase updateProfileInfoUseCase = UpdateProfileInfoUseCase(profileRepository);
-  static final GetChangeRequestsUseCase getChangeRequestsUseCase = GetChangeRequestsUseCase(profileRepository);
-  static final UploadProfilePhotoUsecase uploadProfilePhotoUsecase = UploadProfilePhotoUsecase(profileRepository);
-  static final DecideChangeRequestUseCase decideChangeRequestUseCase = DecideChangeRequestUseCase(profileRepository);
-  static final GetCurrentUserProfileUsecase getCurrentUserProfileUsecase = GetCurrentUserProfileUsecase(profileRepository);
-  static final RequestBeltNicknameChangeUseCase requestBeltNicknameChangeUseCase = RequestBeltNicknameChangeUseCase(profileRepository);
-  
+  static final UpdateProfileInfoUseCase updateProfileInfoUseCase =
+      UpdateProfileInfoUseCase(profileRepository);
+  static final GetChangeRequestsUseCase getChangeRequestsUseCase =
+      GetChangeRequestsUseCase(profileRepository);
+  static final UploadProfilePhotoUsecase uploadProfilePhotoUsecase =
+      UploadProfilePhotoUsecase(profileRepository);
+  static final DecideChangeRequestUseCase decideChangeRequestUseCase =
+      DecideChangeRequestUseCase(profileRepository);
+  static final GetCurrentUserProfileUsecase getCurrentUserProfileUsecase =
+      GetCurrentUserProfileUsecase(profileRepository);
+  static final RequestBeltNicknameChangeUseCase
+  requestBeltNicknameChangeUseCase = RequestBeltNicknameChangeUseCase(
+    profileRepository,
+  );
+
   static final ProfileController profileController = ProfileController(
     getCurrentUserProfileUsecase,
     updateProfileInfoUseCase,
@@ -36,14 +44,11 @@ class ProfileProviders {
     requestBeltNicknameChangeUseCase,
   );
 
-  static final RequestStatusController requestStatusController = RequestStatusController(getChangeRequestsUseCase);
+  static final RequestStatusController requestStatusController =
+      RequestStatusController(getChangeRequestsUseCase);
 
   static List<SingleChildWidget> providers = [
-
     ChangeNotifierProvider(create: (_) => profileController),
-    ChangeNotifierProvider(create: (_) => requestStatusController)
+    ChangeNotifierProvider(create: (_) => requestStatusController),
   ];
-
-
-
 }
